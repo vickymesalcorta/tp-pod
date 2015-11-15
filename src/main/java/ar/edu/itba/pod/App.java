b473d39246cf57070bcfd3b9011bc249a22fe8dc
kpjjpk <juanpabloreyv@gmail.com>
2015-11-15 05:08:03 -0300

    Add basic implementation of console argument parser

--- src/main/java/ar/edu/itba/pod/App.java
+++ src/main/java/ar/edu/itba/pod/App.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.pod;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -22,35 +23,38 @@ import ar.edu.itba.pod.model.ImdbEntry;
 import ar.edu.itba.pod.query.query1.Query1;
 import ar.edu.itba.pod.query.query2.Query2;
 import ar.edu.itba.pod.query.query4.Query4;
+import ar.edu.itba.pod.util.ArgumentParser;
 import io.advantageous.boon.json.JsonFactory;
 import io.advantageous.boon.json.ObjectMapper;
 
 public class App {
-
     private static final String MAP_NAME = "DataMap";
-    private static final String[] DATA_FILES_PATHS = new String[] {
-            "imdb-40.json" };
     private static final String JOB_TRACKER_NAME = "JobTracker";
 
     public static void main(String[] args)
             throws InterruptedException, ExecutionException {
+        ArgumentParser parser = ArgumentParser.builder()
+                .withMandatoryArgument("query").withMandatoryArgument("path")
+                .withOptionalArgument("tope").withOptionalArgument("N")
+                .parse(args);
+        String path = parser.getStringArgument("path");
+
         ClientConfig conf = new ClientConfig();
         HazelcastInstance client = HazelcastClient.newHazelcastClient(conf);
 
         IMap<String, ImdbEntry> map = client.getMap(MAP_NAME);
-        for (String path : DATA_FILES_PATHS) {
-            readMoviesIntoMap(path, map);
-        }
+        readMoviesIntoMap(path, map);
 
         JobTracker tracker = client.getJobTracker(JOB_TRACKER_NAME);
         Job<String, ImdbEntry> job = tracker
                 .newJob(KeyValueSource.fromMap(map));
 
-        int queryN = 4;
+        int queryN = parser.getIntArgument("query");
         // Switch de queries y llamar a la que corresponda
         switch (queryN) {
         case 1:
-            Query1 query1 = new Query1(job, 5);
+            int limit = parser.getIntArgument("N");
+            Query1 query1 = new Query1(job, limit);
             List<Entry<String, Integer>> mostPopularActorsByVotes = query1
                     .evaluate();
             mostPopularActorsByVotes
@@ -58,7 +62,8 @@ public class App {
                             + entry.getKey() + ", Votes: " + entry.getValue()));
             break;
         case 2:
-            Query2 query2 = new Query2(job, 2000);
+            int minYear = parser.getIntArgument("tope");
+            Query2 query2 = new Query2(job, minYear);
             Map<Integer, List<ImdbEntry>> moviesByYear = query2.evaluate();
             moviesByYear.forEach((year, movies) -> movies
                     .forEach(movie -> System.out.println(
@@ -72,14 +77,9 @@ public class App {
                     .evaluate();
             fetishActorsByDirector.forEach((director, actors) -> {
                 String actorListString = actors.stream()
-                        .map(actor -> actor + ",")
-                        .collect(Collectors.joining());
-                System.out
-                        .println(
-                                "Director: " + director + ", Actors: ["
-                                        + actorListString.substring(0,
-                                                actorListString.length() - 2)
-                                + "]");
+                        .collect(Collectors.joining(", "));
+                System.out.println("Director: " + director + ", Actors: ["
+                        + actorListString + "]");
             });
             break;
         }
@@ -87,10 +87,9 @@ public class App {
 
     private static void readMoviesIntoMap(String path,
             IMap<String, ImdbEntry> map) {
-        try (InputStream is = App.class.getClassLoader()
-                .getResourceAsStream(path);
-                InputStreamReader isr = new InputStreamReader(is);
-                LineNumberReader reader = new LineNumberReader(isr)) {
+        try (InputStream is = new FileInputStream(path);
+             InputStreamReader isr = new InputStreamReader(is);
+             LineNumberReader reader = new LineNumberReader(isr)) {
             ObjectMapper mapper = JsonFactory.create();
 
             String line;
---
+++ src/main/java/ar/edu/itba/pod/util/ArgumentParser.java
@@ -0,0 +1,108 @@
+package ar.edu.itba.pod.util;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class ArgumentParser {
+    private static final String TO_IGNORE = "\\s";
+    private static final Pattern EQUALS_PATTERN = Pattern.compile("=");
+
+    private final Map<String, Boolean> optionalityByArgumentName;
+    private final Map<String, String> valuesByArgumentName = new HashMap<>();
+
+    private ArgumentParser(Map<String, Boolean> optionalityByArgumentName) {
+        this.optionalityByArgumentName = optionalityByArgumentName;
+    }
+
+    public int getIntArgument(String name) {
+        String value = getStringArgument(name);
+        try {
+            return Integer.valueOf(value);
+        } catch (NumberFormatException exc) {
+            throw new IllegalArgumentException(
+                    "Invalid format, argument is not a number: " + name);
+        }
+    }
+
+    public String getStringArgument(String name) {
+        String value = valuesByArgumentName.get(name);
+        if (value == null) {
+            throw new IllegalArgumentException("Missing argument: " + name);
+        }
+        return valuesByArgumentName.get(name);
+    }
+
+    private void parse(String... args) {
+        if (!valuesByArgumentName.isEmpty()) {
+            return;
+        }
+        Set<String> mandatoryArguments = optionalityByArgumentName.entrySet()
+                .stream().filter(data -> !data.getValue())
+                .map(data -> data.getKey()).collect(Collectors.toSet());
+
+        Arrays.stream(args).map(ArgumentParser::parseArgument).forEach(arg -> {
+            Boolean optionality = optionalityByArgumentName.get(arg.name);
+            if (optionality == null) {
+                throw new IllegalArgumentException(
+                        "Unexpected argument: " + arg.name);
+            }
+            mandatoryArguments.remove(arg.name);
+            valuesByArgumentName.put(arg.name, arg.value);
+        });
+
+        if (!mandatoryArguments.isEmpty()) {
+            throw new IllegalArgumentException(
+                    "Missing arguments: " + mandatoryArguments.stream()
+                            .collect(Collectors.joining(", ")));
+        }
+    }
+
+    public static ArgumentParserBuilder builder() {
+        return new ArgumentParserBuilder();
+    }
+
+    private static ParsedArgument parseArgument(String rawArgAndValue) {
+        String[] argAndValueArray = EQUALS_PATTERN
+                .split(rawArgAndValue.replace(TO_IGNORE, ""));
+        return new ParsedArgument(argAndValueArray[0], argAndValueArray[1]);
+    }
+
+    public static class ArgumentParserBuilder {
+        private final Map<String, Boolean> optionalityByArgumentName = new HashMap<>();
+
+        public ArgumentParserBuilder withMandatoryArgument(String name) {
+            return withArgument(name, false);
+        }
+
+        public ArgumentParserBuilder withOptionalArgument(String name) {
+            return withArgument(name, true);
+        }
+
+        public ArgumentParser parse(String... args) {
+            ArgumentParser argumentParser = new ArgumentParser(
+                    optionalityByArgumentName);
+            argumentParser.parse(args);
+            return argumentParser;
+        }
+
+        private ArgumentParserBuilder withArgument(String name,
+                boolean optional) {
+            optionalityByArgumentName.put(name, optional);
+            return this;
+        }
+    }
+
+    private static class ParsedArgument {
+        final String name;
+        final String value;
+
+        ParsedArgument(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+    }
+}
